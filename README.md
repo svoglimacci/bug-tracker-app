829be1cf73415f591116fc72ee90a59b4a28b41a
Simon Voglimacci <s.voglimaccI@tuta.io>
2022-08-30 13:26:40 -0400

    added README & screenshots

---
+++ README.md
@@ -0,0 +1,29 @@
+# Bug-tracker-app
+
+Fullstack bug tracking project made as a way to learn Typescript with ReactJS & NodeJS
+
+## Built using
+
+- [ReactJS](https://reactjs.org/)
+- [Material-UI](https://material-ui.com/)
+- [Express.js](https://expressjs.com/)
+- [PostgreSQL](https://www.postgresql.org/)
+- [Docker](https://www.docker.com/)
+
+## Features
+
+- Authentication
+- CRUD: Projects, Issues, Notes
+- Toast notifications
+- Clean Responsive UI
+- Error Handling
+
+## Screenshots
+
+![Login](https://github.com/svoglimacci/bug-tracker-app/tree/main/screenshots/login.jpg)
+![Home](https://github.com/svoglimacci/bug-tracker-app/tree/main/screenshots/Home.jpg)
+![Form](https://github.com/svoglimacci/bug-tracker-app/tree/main/screenshots/NewProject.jpg)
+![Issues](https://github.com/svoglimacci/bug-tracker-app/tree/main/screenshots/Issues.jpg)
+![Notes](https://github.com/svoglimacci/bug-tracker-app/tree/main/screenshots/Notes.jpg)
+
+
---
+++ frontend/src/components/SignupForm.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/destructuring-assignment */
+import * as React from 'react';
+import { useForm, useController, UseControllerProps } from 'react-hook-form';
+import { Typography, TextField, Button, Box } from '@mui/material';
+
+type FormValues = {
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type Props = {
+  onSubmit: (values: { username: string; password: string }) => void;
+};
+
+function Input(props: UseControllerProps<FormValues>) {
+  const { field } = useController(props);
+
+  return (
+    <div>
+      <TextField margin="normal" fullWidth {...field} label={props.name} />
+    </div>
+  );
+}
+
+export default function LoginForm({ onSubmit }: Props) {
+  const { handleSubmit, control } = useForm<FormValues>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        Register
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+        <Input control={control} name="username" rules={{ required: true }} />
+        <Input control={control} name="password" rules={{ required: true }} />
+
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Register
+        </Button>
+      </Box>
+    </Box>
+  );
+}
